68d161747b974c593c2b376ec51869075ae0e3f6
ssyedsaifullah2006-netizen <ssyedsaifullah2006@gmail.com>
2026-09-10 14:44:23 +0530

    Create AmbulanceUnavailableException.java

---
+++ src/main/java/com/dispatch/exception/AmbulanceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.dispatch.exception;
+
+public class AmbulanceUnavailableException extends RuntimeException {
+    public AmbulanceUnavailableException(String message) {
+        super(message);
+    }
+}
